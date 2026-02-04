96c2162bf157df5df489980a67f88ab5a09cf241
Yue XiaoHou <yue@example.com>
2026-02-05 02:42:55 +0800

    在bls_blst.go中补充聚合签名验证内容

--- bls_blst.go
+++ bls_blst.go
@@ -35,3 +35,37 @@ func (b *BlstBLS) Sign(message []byte) ([]byte, error) {
 func (b *BlstBLS) PublicKey() []byte {
     return b.pk.Compress()
 }
+
+// 聚合签名
+func (b *BlstBLS) AggregateSignatures(sigs [][]byte) ([]byte, error) {
+    if len(sigs) == 0 {
+        return nil, nil
+    }
+    var agg blst.P2Aggregate
+    sigObjs := make([]*blst.P2Affine, len(sigs))
+    for i, sbytes := range sigs {
+        sigObj := new(blst.P2Affine)
+        if err := sigObj.Deserialize(sbytes); err != nil {
+            return nil, err
+        }
+        sigObjs[i] = sigObj
+    }
+    agg.Aggregate(sigObjs, true)
+    aggSig := agg.ToAffine()
+    return aggSig.Compress(), nil
+}
+
+// 聚合签名验证
+func (b *BlstBLS) VerifyAggregate(pubKeys [][]byte, message, aggSig []byte) (bool, error) {
+    if aggSig == nil {
+        return false, nil
+    }
+    sig := new(blst.P2Affine).Deserialize(aggSig)
+    pks := make([]*blst.P1Affine, len(pubKeys))
+    for i, pkb := range pubKeys {
+        pks[i] = new(blst.P1Affine).Deserialize(pkb)
+    }
+    const dst = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_"
+    ok := sig.FastAggregateVerify(true, pks, message, []byte(dst))
+    return ok, nil
+}
