2402cf521d92a813cbe138b9f20b119136ea5e3e
Yue XiaoHou <yue@example.com>
2026-02-05 02:54:08 +0800

    部分纠错： ./bls_blst.go:48:25: cannot use err (variable of type *blst.P2Affine) as error value in return statement: *blst.P2Affine does not implement error (missing method Error)

--- bls_blst.go
+++ bls_blst.go
@@ -5,6 +5,8 @@ package main
 
 import (
     "crypto/rand"
+    "errors"
+
     blst "github.com/supranational/blst/bindings/go"
 )
 
@@ -14,28 +16,23 @@ type BlstBLS struct {
     id int
 }
 
-// 新建密钥对
+// 新建 BLS 密钥对
 func NewBlstBLS(id int) *BlstBLS {
     ikm := make([]byte, 32)
-    if _, err := rand.Read(ikm); err != nil {
-        panic(err)
-    }
-    sk := blst.KeyGen(ikm)
+    rand.Read(ikm)
+    sk := blst.KeyGen(ikm) // Note: KeyGen returns *SecretKey
     pk := new(blst.P1Affine).From(sk)
     return &BlstBLS{sk: sk, pk: pk, id: id}
 }
 
+// 单节点签名
 func (b *BlstBLS) Sign(message []byte) ([]byte, error) {
     const dst = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_"
     sig := new(blst.P2Affine).Sign(b.sk, message, []byte(dst))
     return sig.Compress(), nil
 }
 
-func (b *BlstBLS) PublicKey() []byte {
-    return b.pk.Compress()
-}
-
-// 聚合签名
+// 聚合若干签名
 func (b *BlstBLS) AggregateSignatures(sigs [][]byte) ([]byte, error) {
     if len(sigs) == 0 {
         return nil, nil
@@ -43,9 +40,9 @@ func (b *BlstBLS) AggregateSignatures(sigs [][]byte) ([]byte, error) {
     var agg blst.P2Aggregate
     sigObjs := make([]*blst.P2Affine, len(sigs))
     for i, sbytes := range sigs {
-        sigObj := new(blst.P2Affine)
-        if err := sigObj.Deserialize(sbytes); err != nil {
-            return nil, err
+        sigObj := new(blst.P2Affine).Deserialize(sbytes)
+        if sigObj == nil {
+            return nil, errors.New("aggregate: signature deserialize failed")
         }
         sigObjs[i] = sigObj
     }
@@ -54,17 +51,29 @@ func (b *BlstBLS) AggregateSignatures(sigs [][]byte) ([]byte, error) {
     return aggSig.Compress(), nil
 }
 
-// 聚合签名验证
+// 验证聚合签名
 func (b *BlstBLS) VerifyAggregate(pubKeys [][]byte, message, aggSig []byte) (bool, error) {
     if aggSig == nil {
-        return false, nil
+        return false, errors.New("aggSig is nil")
     }
     sig := new(blst.P2Affine).Deserialize(aggSig)
+    if sig == nil {
+        return false, errors.New("aggSig deserialize failed")
+    }
     pks := make([]*blst.P1Affine, len(pubKeys))
     for i, pkb := range pubKeys {
-        pks[i] = new(blst.P1Affine).Deserialize(pkb)
+        pk := new(blst.P1Affine).Deserialize(pkb)
+        if pk == nil {
+            return false, errors.New("pubkey deserialize failed")
+        }
+        pks[i] = pk
     }
     const dst = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_"
     ok := sig.FastAggregateVerify(true, pks, message, []byte(dst))
     return ok, nil
+}
+
+// 获取公钥字节
+func (b *BlstBLS) PublicKey() []byte {
+    return b.pk.Compress()
 }
