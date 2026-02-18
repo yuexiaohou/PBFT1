9b9afdb76be48ffea33ff21c97497e7e6a8461c7
Yue XiaoHou <yue@example.com>
2026-02-18 21:31:17 +0800

    更改了api.js

--- front/src/api.js
+++ front/src/api.js
@@ -1,20 +1,15 @@
 import axios from "axios";
-axios.defaults.baseURL = "/api"; // 代理到你的后端接口
 
-// 登录
-export const login = (username, password) => axios.post("/login", { username, password });
+export const login = (username, password) =>
+    axios.post("/api/login", { username, password });
 
-// 注册
-export const register = (username, password) => axios.post("/register", { username, password });
+export const register = (username, password) =>
+    axios.post("/api/register", { username, password });
 
-// 获取账户余额
-export const getBalance = () => axios.get("/account/balance");
+export const getBalance = () => axios.get("/api/account/balance");
 
-// 账户充值
-export const deposit = (amount) => axios.post("/account/deposit", { amount });
+export const deposit = (amount) => axios.post("/api/account/deposit", { amount });
 
-// 买入/卖出下单
-export const trade = (type, amount) => axios.post("/trade", { type, amount });
+export const trade = (type, amount) => axios.post("/api/trade", { type, amount });
 
-// 查询历史交易
-export const getTradeHistory = () => axios.get("/trade/history");
+export const getTradeHistory = () => axios.get("/api/trade/history");
