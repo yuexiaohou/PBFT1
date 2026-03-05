a0893ee7ecc2070c0bfdafc0985bedf93c21fba4
Yue XiaoHou <yue@example.com>
2026-03-05 23:37:51 +0800

    修改main1.go

--- database/main1.go
+++ database/main1.go
@@ -414,7 +414,7 @@ func main() {
 // === 2026-03-03 高亮新增: 启动时自动模拟撮合轮次（正式项目应由业务流程驱动） ===
 // ==== 2026-03-04 高亮：调用聚合填充所有算法 ====
 	simulateAllAlgos(db, 30, *simMalRatio)
-	fmt.Printf("arr len = %d\n", len(arr) // === 2026-03-03 高亮调试 ===
+	fmt.Printf("arr len = %d\n", len(arr) )// === 2026-03-03 高亮调试 ===
 	for _, rv := range arr {
             fmt.Printf("round stat: %+v\n", rv)
         }
